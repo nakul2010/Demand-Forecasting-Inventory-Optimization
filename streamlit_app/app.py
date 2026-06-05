d9b581d73b0731dec1450577bcfc89606eac760e
nakul2010 <131772815+nakul2010@users.noreply.github.com>
2026-06-05 13:35:03 +0530

    Created initial Streamlit dashboard

---
+++ streamlit_app/app.py
@@ -0,0 +1,15 @@
+import streamlit as st
+import pandas as pd
+
+st.set_page_config(
+    page_title="Demand Forecasting Dashboard",
+    layout="wide"
+)
+
+st.title(
+    "Demand Forecasting & Inventory Optimization"
+)
+
+st.write(
+    "AI-powered demand forecasting and inventory planning dashboard."
+)
